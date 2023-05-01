6a0c2a770c2a5284433df3de9f089e03db50e96b
aesparza84 <aesparza@colum.edu>
2023-05-01 18:26:36 -0500

    more

--- js.js
+++ js.js
@@ -13,7 +13,7 @@ $(window).scroll(function(){
     if(scrollPercent >= 100)
     {
         $('.progress-bar').fadeOut("slow");
-        $('.progress-container').css("background-color", "aqua");
+        $('.progress-container').css("background-color", "white");
     }
     else{
 
@@ -21,8 +21,9 @@ $(window).scroll(function(){
 
         $('.progress-bar').fadeIn("10");
 
-        $('.progress-bar').css("background-image", "linear-gradient(rgb(46, 213, 222), rgb(9, 107, 73) , rgb(4, 172, 94))");
+        $('.progress-bar').css("background-image", "linear-gradient(rgb(84, 222, 46), rgb(94, 107, 9) , rgb(172, 63, 4));");
 
     }
-
 })
+
+$(figcaption).fadeIn(slow);
