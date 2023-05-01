5484236a157eae2d556d44147f38351ddd0ec6d2
aesparza84 <aesparza@colum.edu>
2023-05-01 16:16:10 -0500

    2nd page

--- js.js
+++ js.js
@@ -1,12 +1,28 @@
 $(window).scroll(function(){
 
     
-    let pgheight = $(document).height();
-    let scrollammount = $(document).scrollTop();
-    let scrollpercent = (scrollammount/pgheight)*100;
+    let pageHeight = $(document).height() - $(window).height();
+    let amountScrolled = $(document).scrollTop();
+    
+    let scrollPercent = (amountScrolled / pageHeight) * 100;
+    
+    $('.progress-bar').css("height", scrollPercent + "%");
 
-    console.log(scrollpercent);
+    console.log(scrollPercent);
 
-    $('.progress-bar').css("height", scrollpercent+"%");
+    if(scrollPercent >= 100)
+    {
+        $('.progress-bar').fadeOut("slow");
+        $('.progress-container').css("background-color", "aqua");
+    }
+    else{
+
+        $('.progress-container').css("background-color", "transparent");
+
+        $('.progress-bar').fadeIn("10");
+
+        $('.progress-bar').css("background-image", "linear-gradient(rgb(46, 213, 222), rgb(9, 107, 73) , rgb(4, 172, 94))");
+
+    }
 
 })
